c78ee74b0af19134a79c5108b67fe0920195c269
Quarx2k <agent00791@gmail.com>
2014-07-16 21:47:42 +0700

    Revert "accelerate camera recording start up speed"
    This reverts commit fd1ef6154d9c508348f8e35b34bc0de7d4c91c86.

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -5213,13 +5213,13 @@ status_t OMXCodec::read(
             mPaused = false;
         }
 
+        drainInputBuffers();
 
         if (mState == EXECUTING) {
             // Otherwise mState == RECONFIGURING and this code will trigger
             // after the output port is reenabled.
             fillOutputBuffers();
         }
-        drainInputBuffers();
     }
 
     if (seeking) {
